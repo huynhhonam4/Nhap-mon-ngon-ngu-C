e528c2e132d2ff89498702cdda5fe03dc0392b1d
Huỳnh Hồ Nam <74333373+huynhhonam4@users.noreply.github.com>
2021-01-28 14:20:35 +0700

    Update ham_03.c

--- Bài tập 5/ham_03.c
+++ Bài tập 5/ham_03.c
@@ -1,64 +1,64 @@
 #include<stdio.h>
-void Nhap_pt_mang(int n, int a[])
-{
-    for (int i = 0; i < n; i++)
-    {
+
+void Nhap_pt_mang(int a[]) {
+  int n;
+  printf ("nhap n = ");
+  scanf ("%d", &n);
+    for (int i = 0; i < n; i++) {
         printf("a[%d] = ", i);
         scanf("%d", &a[i]);
     }
-    
 }
-//nhập các phần tử trong mảng
+//nhap n va a[] từ bàn phím
 
-void Hien_thi_pt_mang(int n, int a[])
-{
-    printf("Cac phan tu trong mang: ");
-    for (int i = 0; i < n; i++)
-    {
+void Hien_thi_pt_mang(int a[]) {
+  int n;
+    printf("Cac phan tu trong mang = ");
+    for (int i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
-    
 }
-//hiển thị các phần tử có trong mảng
-    
-void Hien_thi_tong_chan(int n, int a[])
-{
+//hiển thị các phần tử trong mảng đã nhập ra màn hình
+
+void Hien_thi_tong_chan(int a[]) {
+  int n;
     int S = 0;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         if(a[i] % 2 == 0) {
             S += a[i];
         }
     }
-    printf("\nTong cac so chan la: %d", S); 
-}
-//tổng chẵn lẽ của các phần tử có trong mảng
-
-void Dem_chan_le_0(int n, int a[])
-{
-    int demchan = 0, demle = 0, dem0 = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if(a[i] % 2 == 0)
-            demchan++;
-        if(a[i] % 2 == 1)
-            demle++;
-        if(a[i] == 0)
-            dem0++;
-    }
-    printf("\nCo tat ca %d so chan, %d so le va %d so 0", demchan, demle, dem0);
+    printf("\nTong cac so chan  = %d", S);
     
 }
-//đếm các số chẵn và số lẻ có trong mảng
+//hiển thị tổng của các số chẵn
 
-int main(){
+void Dem_chan_le_0(int a[]) {
+  int n;
+    int demchan = 0, demle = 0, dem0 = 0;
+    for (int i = 0; i < n; i++) {
+        if(a[i] % 2 == 0 && a[i] != 0) {
+          demchan++;
+        }
+        if(a[i] % 2 != 0) {
+          demle++;
+        }
+        if(a[i] == 0) {
+          dem0++;
+        }
+    }
+    printf("\nDem so chan = %d\nDem so le = %d\nDem so 0 = %d", demchan, demle, dem0);
+}
+//hiểm thị đếm chẵn, đếm lẻ và đếm số 0 
+
+int main() {
   int n;
   printf("Nhap n = ");
   scanf("%d", &n);
   int a[n];
-  Nhap_pt_mang(n, a);
-  Hien_thi_pt_mang(n, a);
-  Hien_thi_tong_chan(n, a);
-  Dem_chan_le_0(n, a);
+  Nhap_pt_mang(a);
+  Hien_thi_pt_mang(a);
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
   return 0;
 }
